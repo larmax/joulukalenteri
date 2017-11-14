6778cdb40654722e6b571e17da3c198c3aafdf99
kimnikander <nikander.kim@gmail.com>
2017-11-14 11:59:08 +0200

    kuvanvaihtaminen

--- JouluKalenteri/src/joulukalenteri/MainJouluKalenteri.java
+++ JouluKalenteri/src/joulukalenteri/MainJouluKalenteri.java
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author s1601402
  */
-public class JouluKalenteri extends Application {
+public class MainJouluKalenteri extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
