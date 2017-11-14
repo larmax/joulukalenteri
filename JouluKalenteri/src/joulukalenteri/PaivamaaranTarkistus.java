2154a9c1ac8f881e133fea8ec7e03932076b3ff5
Miller Leon <s1601385@student.businesscollege.fi>
2017-11-14 12:02:44 +0200

    pvm kesken

---
+++ JouluKalenteri/src/joulukalenteri/PaivamaaranTarkistus.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package joulukalenteri;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class PaivamaaranTarkistus {
+    private Date tamaPaiva = new Date();
+ 
+
+
+private boolean after(String date){
+   
+    //Päivämäärät tulee olla tässä muodossa
+    Date eka = new Date();    
+  
+        
+        
+        //Muunnetaan String dateksi, jotta voimme myöhemmin vertailla.
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+          eka = sdf.parse(date);
+            
+        } catch (ParseException ex) {
+            
+        }
+ if(tamaPaiva.after(eka)){
+     return true;
+ }else{
+   return false;  
+ }
+        
+}
+
+}
+
