f0b408a79c42b364491cee116a47f9b9c228c57e
Leon Miller <s1601385@student.businesscollege.fi>
2017-11-26 21:12:08 +0200

    poistettu turhaa

--- JouluKalenteri/src/joulukalenteri/MainJouluKalenteri.java
+++ JouluKalenteri/src/joulukalenteri/MainJouluKalenteri.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package joulukalenteri;
 
 import javafx.application.Application;
@@ -15,39 +10,23 @@ import java.util.Date;
 import java.util.ArrayList;
 
 /**
- *
- * @author s1601402
+ * @author s1601402 and s1601385
  */
 public class MainJouluKalenteri extends Application {
 
-    
-       static int luukutt;
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
-              PaivamaaranTarkistus pvm = new PaivamaaranTarkistus();
-    
-            
-        }
-        
-//  KuvanVaihtaminen VaihaKuva = new KuvanVaihtaminen(paivat);
-//    FXMLDocumentController controlleri = new FXMLDocumentController(luukut);
-
 
     }
-    
 
-    
-
+}
--- JouluKalenteri/src/joulukalenteri/PaivamaaranTarkistus.java
+++ JouluKalenteri/src/joulukalenteri/PaivamaaranTarkistus.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package joulukalenteri;
 
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * @author s1601385
+ */
 public class PaivamaaranTarkistus {
 
     int luukut = 1;
@@ -21,23 +19,19 @@ public class PaivamaaranTarkistus {
     int luukut() {
         Date eka = new Date();
 
-//        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-//String s = sdf.format(date);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(eka);
 
-   Calendar cal = Calendar.getInstance();
-cal.setTime(eka);
-
-int day = cal.get(Calendar.DAY_OF_MONTH);
-int kk = cal.get(Calendar.MONTH)+ 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int kk = cal.get(Calendar.MONTH) + 1;
         for (int i = 1; i < day; i++) {
-if(kk == 12){
-    luukut += 1; 
-}else{
-        luukut = 0;        
-                }
-        
-        
+            if (kk == 12) {
+                luukut += 1;
+            } else {
+                luukut = 0;
+            }
+
+        }
+        return luukut;
     }
-return luukut;
-}
 }
